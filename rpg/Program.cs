dbfcc6112cc59047c9e48beac0bd816018704e71
Harlow Lee <leeh@rsd407.org>
2023-05-01 20:39:12 -0700

    write intro

--- rpg/Program.cs
+++ rpg/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace rpg
+﻿/*
+rpg by harlow
+
+Pseudocode
+	GS1 
+	Output intro to the screen
+Maintenance Log:
+05/1/2023	start
+            write intro
+			
+*/
+
+
+namespace rpg
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiveInstructions();
+
+        }
+
+        static void GiveInstructions()
+        {
+            Console.WriteLine("hi welcome to school ");
+            Console.WriteLine("ulost stuff from ur bag go find ur things ");
+            Console.WriteLine("code by harlow");
         }
     }
 }
