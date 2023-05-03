20d1c95bcaaa8f38a7e306af4b1cca872d3dab09
Harlow Lee <leeh@rsd407.org>
2023-05-02 19:31:17 -0700

    goalsheet 3 1

--- rpg/Program.cs
+++ rpg/Program.cs
@@ -7,9 +7,16 @@ Pseudocode
 Maintenance Log:
 05/1/2023	start
             write intro
+
+5/2/2023    make player class
+            wrote hp and damage
+            make name
+
 			
 */
 
+
+using System.Security.Cryptography;
 
 namespace rpg
 {
@@ -27,5 +34,24 @@ namespace rpg
             Console.WriteLine("ulost stuff from ur bag go find ur things ");
             Console.WriteLine("code by harlow");
         }
+    }
+
+    internal class Player
+    {
+
+       static Random rng = new Random();
+       static int minHp = 5, maxHp = 26;
+
+
+        int hp = minHp + rng.Next(maxHp - minHp);
+
+        static int minDamage = 1, maxDamage = 6;
+
+        int damage = minDamage + rng.Next(maxDamage - minDamage);
+
+        string name = "";
+
+
+
     }
 }
